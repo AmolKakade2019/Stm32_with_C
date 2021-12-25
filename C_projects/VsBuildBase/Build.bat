5b566ae66ba47214fcd018463e31ddb8ff699d81
AmolKakade <kakade.amol.v@gmail.com>
2021-12-25 14:01:19 +0100

    - minor cleanup - Amol.

--- C_projects/VsBuildBase/Build.bat
+++ C_projects/VsBuildBase/Build.bat
@@ -1,10 +1,14 @@
 echo
+echo BATCH: -------------------- Batch START           --------------- 
 @echo %1
 @echo %2
 set BUILD_CMD=%1
 set PrjConfig=%2
 @echo 
-echo ------ Batch Arguments ----%BUILD_CMD%,%PrjConfig%,
+echo BATCH: -------------------- Batch Arguments START --------------- 
+echo BATCH: %BUILD_CMD%,%PrjConfig%,
+echo BATCH: -------------------- Batch Arguments END   --------------- 
 Make -C ./Make/ -f Makefile.gmk %BUILD_CMD% VSConfig=%PrjConfig%
 echo "Batch Finished"
 echo "creating dumps"
+echo BATCH: -------------------- Batch END            --------------- 
